3f195a00dc140a84d3b3cdc8770fa04cb6aeca4c
Chonlasak Tumna <137064083+ChonlasakThamana@users.noreply.github.com>
2023-10-04 16:56:53 +0700

    12-4

--- Homework/HW12/hw12-4-promote-new-salary.cpp
+++ Homework/HW12/hw12-4-promote-new-salary.cpp
@@ -88,4 +88,63 @@
         The Employee get x2.0 is Aey
         The Employee get x1.0 is Orio
 */
+#include <stdio.h>
+#include <string.h>
+
+int main() {
+    char name[100];
+    int exp;
+    int x1_0_count = 0, x1_2_count = 0, x1_4_count = 0, x1_6_count = 0, x1_8_count = 0, x2_0_count = 0;
+    char x2_0_name[100], x1_0_name[100];
+    
+    while (1) {
+        printf("Employee Name (enter -1 to exit): ");
+        scanf("%s", name);
+        
+        if (strcmp(name, "-1") == 0) {
+            break;
+        }
+        
+        printf("EXP: ");
+        scanf("%d", &exp);
+        
+        if (exp >= 0 && exp <= 1000) {
+            printf("%s gets x1.0\n", name);
+            x1_0_count++;
+            if (exp > 0) {
+                strcpy(x1_0_name, name);
+            }
+        } else if (exp <= 2000) {
+            printf("%s gets x1.2\n", name);
+            x1_2_count++;
+        } else if (exp <= 3000) {
+            printf("%s gets x1.4\n", name);
+            x1_4_count++;
+        } else if (exp <= 4000) {
+            printf("%s gets x1.6\n", name);
+            x1_6_count++;
+        } else if (exp <= 5000) {
+            printf("%s gets x1.8\n", name);
+            x1_8_count++;
+        } else {
+            printf("%s gets x2.0\n", name);
+            x2_0_count++;
+            if (exp > 0) {
+                strcpy(x2_0_name, name);
+            }
+        }
+    }
+    
+    printf("**** Salary Result ****\n");
+    printf("x1.0 count %d\n", x1_0_count);
+    printf("x1.2 count %d\n", x1_2_count);
+    printf("x1.4 count %d\n", x1_4_count);
+    printf("x1.6 count %d\n", x1_6_count);
+    printf("x1.8 count %d\n", x1_8_count);
+    printf("x2.0 count %d\n", x2_0_count);
+    printf("The Employee get x2.0 is %s\n", x2_0_name);
+    printf("The Employee get x1.0 is %s\n", x1_0_name);
+    
+    return 0;
+}
 
